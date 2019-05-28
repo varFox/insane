dc453355e6dc8006947df88d2faec7f95a00e7c2
varFox <31412438+varFox@users.noreply.github.com>
2019-05-28 14:34:18 +0300

    6

--- src/js/script.js
+++ src/js/script.js
@@ -35,7 +35,7 @@ window.addEventListener('DOMContentLoaded', function () {
     
 
     showUpSlider('.showup__content-card', '.slick-prev.showup__btn', '.slick-next.showup__btn', 'card-active', false);
-    showUpSlider('.modules__content-card', '.modules__info .slick-prev', '.modules__info .slick-next', 'card-active', false);//true);
+    showUpSlider('.modules__content-card', '.modules__info .slick-prev', '.modules__info .slick-next', 'card-active', true);//true);
     showUpSlider('.feed__slider-cards', '.feed__info-btns .slick-prev', '.feed__info-btns .slick-next', 'feed__item-active', false, true);
     linkSlide();
     clickToShow();
